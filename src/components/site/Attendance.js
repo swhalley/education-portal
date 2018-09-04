da80c1c57b77c9fc4a6f53990fd96cce126c481e
Sean Whalley <swhalley@gmail.com>
2018-09-03 22:45:26 -0300

    added info message warning of bad data

--- src/components/site/Attendance.js
+++ src/components/site/Attendance.js
@@ -1,14 +1,73 @@
 import React, { Component, Fragment } from "react";
+
+import InfoIcon from "@material-ui/icons/Info";
+import IconButton from "@material-ui/core/IconButton";
+import CloseIcon from "@material-ui/icons/Close";
+
 import SchoolsTable from "../school/SchoolsTable";
 import AttendanceBlob from "../school/about/AttendanceBlob";
+import { withStyles, SnackbarContent, Snackbar } from "@material-ui/core";
 
-export default class Attendance extends Component {
+const styles = theme => ({
+  message: {
+    display: "flex",
+    alignItems: "center"
+  },
+  primary: {
+    backgroundColor: theme.palette.primary.light,
+    maxWidth: "initial"
+  },
+  icon: {
+    opacity: 0.9,
+    marginRight: theme.spacing.unit
+  },
+  wrapper: {
+    width: "110%"
+  }
+});
+
+class Attendance extends Component {
+  state = {
+    infoOpen: true
+  };
+
   render() {
+    const { classes, ...other } = this.props;
+
     return (
       <Fragment>
         <AttendanceBlob />
-        <SchoolsTable {...this.props} />
+        <Snackbar
+          open={this.state.infoOpen}
+          autoHideDuration={10000}
+          onClose={() => this.setState({ infoOpen: false })}
+          className={classes.wrapper}
+        >
+          <SnackbarContent
+            className={classes.primary}
+            message={
+              <span className={classes.message}>
+                <InfoIcon className={classes.icon} />
+                Recent data is missing for 2016 and 2017 for a lot of schools.
+                Totals and trends are not available.
+              </span>
+            }
+            action={[
+              <IconButton
+                key="close"
+                color="inherit"
+                className={classes.close}
+                onClick={() => this.setState({ infoOpen: false })}
+              >
+                <CloseIcon className={classes.icon} />
+              </IconButton>
+            ]}
+          />
+        </Snackbar>
+        <SchoolsTable {...other} />
       </Fragment>
     );
   }
 }
+
+export default withStyles(styles)(Attendance);
